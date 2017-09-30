c661823c6e1984b835c9d9218e1a1b06ef2227b7
Tim Berman <tim.berman@192-168-1-107.tpgi.com.au>
2017-09-30 17:13:41 +1000

    make proper component

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,48 +2,74 @@ import React from 'react';
 import { Textfield, Button } from 'react-mdl';
 import { Config } from "../config";
 
-const email = "berman.tim@gmail.com";
-let password = "";
+export default class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "berman.tim@gmail.com",
+      password: ""
+    };
 
-function login() {
-  let headers = new Headers();
-  headers.append("Content-Type", "application/json");
-  let fetchData = {
-      method: 'POST',
-      body: JSON.stringify({ email: email, password: password}),
-      headers: headers
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
-  fetch(Config.apiUrl + "login", fetchData)
-    .then((response) => response.json())
-    .then(data => {
-      Config.token = data._id;
-      Config.userEmail = data.local.email;
-      Config.userId = data._id;
-      window.location.href = "#/list";
-    })
-}
 
-export default () => (
-  <div>
-    <h1>Login</h1>
-    <div>
-      <Textfield
-        onChange={() => {}}
-        label="Email"
-        style={{width: '200px'}}
-        value={ email }
-      />
-    </div>
-    <div>
-      <Textfield
-        onChange={(e) => {password = e.target.value}}
-        label="Password"
-        style={{width: '200px'}}
-        password
-      />
-    </div>
-    <div>
-      <Button raised colored onClick={login}>Sign in</Button>
-    </div>
-  </div>
-);
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  login() {
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    let fetchData = {
+        method: 'POST',
+        body: JSON.stringify(this.state),
+        headers: headers
+    }
+
+    fetch(Config.apiUrl + "login", fetchData)
+      .then((response) => response.json())
+      .then(data => {
+        Config.token = data._id;
+        Config.userEmail = data.local.email;
+        Config.userId = data._id;
+        window.location.href = "#/list";
+      })
+  }
+
+  render() {
+    return (
+        <div>
+          <h1>Login</h1>
+          <div>
+            <Textfield
+              name="email"
+              onChange={ this.handleInputChange }
+              label="Email"
+              style={{width: '200px'}}
+              defaultValue={ this.state.email }
+            />
+          </div>
+          <div>
+            <Textfield
+              name="password"
+              onChange={ this.handleInputChange }
+              label="Password"
+              style={{width: '200px'}}
+              defaultValue={ this.state.password }
+              password
+            />
+          </div>
+          <div>
+            <Button raised colored onClick={ (e) => {this.login(e)} }>Sign in</Button>
+          </div>
+        </div>
+    )
+  }
+}
